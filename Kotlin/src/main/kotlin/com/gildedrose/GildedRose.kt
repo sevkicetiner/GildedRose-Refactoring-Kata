13d0bc65cd6622af5edef7e90c042ad9e2adc255
ctnr <sevki.ctnr@gmail.com>
2024-05-07 03:16:53 +0200

    GildedRose refactored by requirements and written unit tests

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,57 @@
 package com.gildedrose
 
+import kotlin.math.max
+
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        items.forEach { item->
+            when(item.name){
+                "Aged Brie"-> updateAgedBrideQuality(item)
+                "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePassQuality(item)
+                "Sulfuras, Hand of Ragnaros" -> {} // quality is not need to update in this situation
+                else -> updateRegularItemQuality(item)
             }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+            updateSellIn(item)
         }
     }
 
+    private fun updateRegularItemQuality(item: Item) {
+        //there are 2 rule about 'twice as fast as normal'
+        if(item.name.startsWith("Conjured") || item.sellIn <= 0){
+            decreaseQuality(item, 2)
+        } else {
+            decreaseQuality(item)
+        }
+    }
+
+    private fun updateBackstagePassQuality(item: Item) {
+        // there are 4 different situation for Backstage passes
+        when{
+            item.sellIn <= 0 -> item.quality = 0
+            item.sellIn <= 5 -> increaseQuality(item, 3)
+            item.sellIn <= 10 -> increaseQuality(item,2)
+            else -> increaseQuality(item)
+        }
+
+    }
+
+    private fun updateSellIn(item: Item) {
+        item.sellIn--
+    }
+
+    private fun updateAgedBrideQuality(item: Item) {
+        increaseQuality(item)
+    }
+
+    private fun increaseQuality(item:Item, amount:Int = 1){
+        // quality can't be bigger than 50
+        item.quality = minOf(item.quality + amount, 50)
+    }
+
+    private fun decreaseQuality(item:Item, amount: Int = 1){
+        //quality can't be lower then 0
+        item.quality = max(0,item.quality - amount)
+    }
 }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -14,6 +14,75 @@ internal class GildedRoseTest {
 
     }
 
+    @Test
+    fun `updateQuality() decreases sellIn and quality correctly for regular items` (){
+        val items = listOf(Item("Regular item", 12, 22))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(11, items[0].sellIn)
+        assertEquals(20, items[0].quality)
+    }
+
+    @Test
+    fun `updateQuality() increases quality correctly for Aged Brie` () {
+        val items = listOf(Item("Aged Brie", 7, 12))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(items[0].quality, 13)
+        assertEquals(items[0].sellIn, 6)
+    }
+
+    @Test
+    fun `updateQuality() set quality to 0 for expired Backstage Passed` (){
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 7))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(0, items[0].quality)
+    }
+
+    @Test
+    fun `updateQuality() increases quality correctly for Backstage Passes within 5 days to concert` (){
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 4, 12))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(3, items[0].sellIn)
+        assertEquals(15, items[0].quality)
+    }
+
+    @Test
+    fun `updateQuality() increases quality correctly for Backstage Passes within 10 days to concert` (){
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 8, 8))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(7, items[0].sellIn)
+        assertEquals(10, items[0].quality)
+    }
+
+    @Test
+    fun `updateQuality() decreases quality twice as fast for Conjured items`(){
+        val items = listOf(Item("Conjured item", 7, 8))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(6, items[0].sellIn)
+        assertEquals(6, items[0].quality)
+    }
+
+    @Test
+    fun `updateQuality() decreases quality twice as fast for passed of date items`(){
+        val items = listOf(Item("Conjured item", -7, 8))
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(-8, items[0].sellIn)
+        assertEquals(6, items[0].quality)
+    }
+
+    @Test
+    fun `updateQuality() never decreases quality for Sulfuras` () {
+        val items = listOf(Item("Sulfuras, Hand of Ragnaros", 0, 80), )
+        //simulation for 1 day
+        GildedRose(items).updateQuality()
+        assertEquals(80, items[0].quality)
+    }
 }
 
 
